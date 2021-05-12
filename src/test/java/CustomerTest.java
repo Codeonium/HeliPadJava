b19ccd9358f13db3f069208b4e2536762f2d0c3c
codeonium <codeon2020@gmail.com>
2021-05-12 15:25:16 +0100

    dealer and customer tests started

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -2,12 +2,19 @@ import PeopleAndSales.Customer;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
     Customer customer;
 
     @Before
     public void setUp() {
         customer = new Customer (700.00);
+    }
+
+    @Test
+    public void hasMoney() {
+        assertEquals(700.00, customer.getMoney(),0.01);
     }
 }
 
--- src/test/java/DealershipTest.java
+++ src/test/java/DealershipTest.java
@@ -1,2 +1,20 @@
+import PeopleAndSales.Customer;
+import PeopleAndSales.Dealership;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DealershipTest {
+    Dealership dealer;
+
+    @Before
+    public void setUp() {
+        dealer = new Dealership (70000.00);
+    }
+
+    @Test
+    public void hasMoney() {
+        assertEquals(70000.00, dealer.getTill(),0.01);
+    }
 }
